72cacae3d50a220f6238e721ee0b6523df570639
Aron Steg <aron.steg+github@bcde.com.au>
2017-03-17 17:11:05 +1100

    Added time stamps to logs.

--- echo_server.js
+++ echo_server.js
@@ -6,50 +6,65 @@
  */
 
 const DEFAULT_PORT = 60000;
-
-
 var net = require('net');
-var log = function(who, what) {
+var cid = 1;
+var started = new Date().getTime() / 1000;
+var port = parseInt(process.argv[2]);
+if (port <= 1024 || isNaN(port)) port = DEFAULT_PORT;
+
+
+function log2(who, what) {
     return function() {
         var args = Array.prototype.slice.call(arguments);
-        // console.log('[%s on %s]', who, what, args);
+        // log('[%s on %s]', who, what, args);
     };
 };
 
 
-var cid = 1;
-var echo = function(socket) {
+function log() {
+    var now = new Date().getTime() / 1000;
+    var time = parseInt((now - started) % 10000);
+    var args = Array.prototype.slice.call(arguments);
+    args[0] = "[%d] " + args[0];
+    args.splice(1, 0, time);
+    console.log.apply(null, args);
+}
+
+function echo(socket) {
 
     socket.id = cid++;
 
     socket.on('data', function(data) {
-        console.log("Data[%d]: %s", socket.id, data.toString().trim());
+        log("Data[%d]: %s", socket.id, data.toString().trim());
     });
 
     socket.on('timeout', function() {
         socket.destroy();
     });
 
+    socket.on('error', function() {
+        socket.destroy();
+    });
+
+    socket.on('end', function() {
+        socket.destroy();
+    });
+
     socket.on('close', function() {
         server.getConnections(function(err, count) {
-            console.log("Disconnection from %s:%d, %d of %d open connections", socket.remoteAddress, socket.remotePort, socket.id, count);
+            log("Disconnection from %s:%d, %d of %d open connections", socket.remoteAddress, socket.remotePort, socket.id, count);
         })
     });
 
-    socket.on('end', log('socket', 'end'));
-    socket.on('drain', log('socket', 'drain'));
-    socket.on('error', log('socket', 'error'));
+    socket.on('drain', () => null);
 
     socket.pipe(socket);
 
     server.getConnections(function(err, count) {
-        console.log("Connection from %s:%d to %s:%d, %d of %d open connections", socket.remoteAddress, socket.remotePort, socket.localAddress, socket.localPort, socket.id, count);
+        log("Connection from %s:%d to %s:%d, %d of %d open connections", socket.remoteAddress, socket.remotePort, socket.localAddress, socket.localPort, socket.id, count);
     });
 };
 
-
-var port = parseInt(process.argv[2]);
-if (port <= 1024 || isNaN(port)) port = DEFAULT_PORT;
 
 /**
  *  net.Server (http://nodejs.org/api/net.html#net_class_net_server)
@@ -61,7 +76,7 @@ server.listen({ host: "0.0.0.0", port: port });
 
 server.on('listening', function() {
     var ad = server.address();
-    console.log('Listening on %s:%s using %s', ad.address, ad.port, ad.family);
+    log('Listening on %s:%s using %s', ad.address, ad.port, ad.family);
 });
 
 server.on('connection', function(socket) {
@@ -70,6 +85,6 @@ server.on('connection', function(socket) {
 });
 
 server.on('error', function(err) {
-    console.log(err);
-    server.close(function() { console.log("shutting down the server!"); });
+    log(err);
+    server.close(function() { log("shutting down the server!"); });
 });
